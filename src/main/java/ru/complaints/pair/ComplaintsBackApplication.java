6538fb673b5617e84f895cdfbf75bcdbd99577a2
Anastasiya <nastya050620031n@gmail.com>
2025-10-05 21:10:17 +0700

    initial commit

---
+++ src/main/java/ru/complaints/pair/ComplaintsBackApplication.java
@@ -0,0 +1,13 @@
+package ru.complaints.pair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/complaints/pair/ComplaintsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.complaints.pair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
